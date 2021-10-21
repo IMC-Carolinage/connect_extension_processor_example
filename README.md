36ea89b944a11250476dc6c5660b614181803ec4
cgimene00 <carolina.gimenezescalante@ingrammicro.com>
2021-10-21 13:10:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Welcome to connect_extension_processor_example !
+
+
+Processor Example using a Mock API Endpoint
+
+
+
+## License
+
+**connect_extension_processor_example** is licensed under the *Apache Software License 2.0* license.
+
---
+++ tests/test_connect_extension_processor_example.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021, Carolina Giménez Escalante
+# All rights reserved.
+#
+
+from connect_ext.extension import Connect_extension_processor_exampleExtension
+
+
+def test_process_asset_purchase_request(
+    sync_client_factory,
+    response_factory,
+    logger,
+):
+    config = {}
+    request = {'id': 1}
+    responses = [
+        response_factory(count=100),
+        response_factory(value=[{'id': 'item-1', 'value': 'value1'}]),
+    ]
+    client = sync_client_factory(responses)
+    ext = Connect_extension_processor_exampleExtension(client, logger, config)
+    result = ext.process_asset_purchase_request(request)
+    assert result.status == 'success'
+
+
+def test_process_asset_change_request(
+    sync_client_factory,
+    response_factory,
+    logger,
+):
+    config = {}
+    request = {'id': 1}
+    responses = [
+        response_factory(count=100),
+        response_factory(value=[{'id': 'item-1', 'value': 'value1'}]),
+    ]
+    client = sync_client_factory(responses)
+    ext = Connect_extension_processor_exampleExtension(client, logger, config)
+    result = ext.process_asset_change_request(request)
+    assert result.status == 'success'
+
+
+def test_process_asset_suspend_request(
+    sync_client_factory,
+    response_factory,
+    logger,
+):
+    config = {}
+    request = {'id': 1}
+    responses = [
+        response_factory(count=100),
+        response_factory(value=[{'id': 'item-1', 'value': 'value1'}]),
+    ]
+    client = sync_client_factory(responses)
+    ext = Connect_extension_processor_exampleExtension(client, logger, config)
+    result = ext.process_asset_suspend_request(request)
+    assert result.status == 'success'
+
+
+def test_process_asset_resume_request(
+    sync_client_factory,
+    response_factory,
+    logger,
+):
+    config = {}
+    request = {'id': 1}
+    responses = [
+        response_factory(count=100),
+        response_factory(value=[{'id': 'item-1', 'value': 'value1'}]),
+    ]
+    client = sync_client_factory(responses)
+    ext = Connect_extension_processor_exampleExtension(client, logger, config)
+    result = ext.process_asset_resume_request(request)
+    assert result.status == 'success'
+
+
+def test_process_asset_cancel_request(
+    sync_client_factory,
+    response_factory,
+    logger,
+):
+    config = {}
+    request = {'id': 1}
+    responses = [
+        response_factory(count=100),
+        response_factory(value=[{'id': 'item-1', 'value': 'value1'}]),
+    ]
+    client = sync_client_factory(responses)
+    ext = Connect_extension_processor_exampleExtension(client, logger, config)
+    result = ext.process_asset_cancel_request(request)
+    assert result.status == 'success'
+
+
+def test_process_asset_adjustment_request(
+    sync_client_factory,
+    response_factory,
+    logger,
+):
+    config = {}
+    request = {'id': 1}
+    responses = [
+        response_factory(count=100),
+        response_factory(value=[{'id': 'item-1', 'value': 'value1'}]),
+    ]
+    client = sync_client_factory(responses)
+    ext = Connect_extension_processor_exampleExtension(client, logger, config)
+    result = ext.process_asset_adjustment_request(request)
+    assert result.status == 'success'
+
+
+def test_validate_asset_purchase_request(
+    sync_client_factory,
+    response_factory,
+    logger,
+):
+    config = {}
+    request = {'id': 1}
+    responses = [
+        response_factory(count=100),
+        response_factory(value=[{'id': 'item-1', 'value': 'value1'}]),
+    ]
+    client = sync_client_factory(responses)
+    ext = Connect_extension_processor_exampleExtension(client, logger, config)
+    result = ext.validate_asset_purchase_request(request)
+    assert result.status == 'success'
+    assert result.data == request
+
+
+def test_validate_asset_change_request(
+    sync_client_factory,
+    response_factory,
+    logger,
+):
+    config = {}
+    request = {'id': 1}
+    responses = [
+        response_factory(count=100),
+        response_factory(value=[{'id': 'item-1', 'value': 'value1'}]),
+    ]
+    client = sync_client_factory(responses)
+    ext = Connect_extension_processor_exampleExtension(client, logger, config)
+    result = ext.validate_asset_change_request(request)
+    assert result.status == 'success'
+    assert result.data == request
