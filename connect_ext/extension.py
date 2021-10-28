fe2cba2f1546b92c95f7ea2b250544385fc13c79
cgimene00 <carolina.gimenezescalante@ingrammicro.com>
2021-10-28 10:46:12 +0200

    First version with the use cases

--- connect_ext/extension.py
+++ connect_ext/extension.py
@@ -8,40 +8,57 @@ from connect.eaas.extension import (
     ProcessingResponse,
     ValidationResponse,
 )
+from connect_ext.app.purchase import (
+    Purchase,
+    Change,
+    Cancel,
+    Resume,
+    Suspend)
 
 
-class Connect_extension_processor_exampleExtension(Extension):
+class ConnectExtensionProcessorExampleExtension(Extension):
+
+    ACTIVATION_TEMPLATE_NAME = 'Default Activation Template'
+    TIER_CONFIG_ACTIVATION_TEMPLATE_NAME = 'Default Activation Template'
+    API_ENDPOINT = ''
 
     def process_asset_purchase_request(self, request):
         self.logger.info(f"Obtained request with id {request['id']}")
+        if request['status'] == 'pending':
+            ConnectExtensionProcessorExampleExtension.ACTIVATION_TEMPLATE_NAME = self.config['ACTIVATION_TEMPLATE_NAME']
+            ConnectExtensionProcessorExampleExtension.API_ENDPOINT = self.config['API_ENDPOINT']
+            return Purchase().process_request(request, self.client)
         return ProcessingResponse.done()
 
     def process_asset_change_request(self, request):
         self.logger.info(f"Obtained request with id {request['id']}")
+        if request['status'] == 'pending':
+            ConnectExtensionProcessorExampleExtension.ACTIVATION_TEMPLATE_NAME = self.config['ACTIVATION_TEMPLATE_NAME']
+            ConnectExtensionProcessorExampleExtension.API_ENDPOINT = self.config['API_ENDPOINT']
+            return Change().process_request(request, self.client)
         return ProcessingResponse.done()
 
     def process_asset_suspend_request(self, request):
         self.logger.info(f"Obtained request with id {request['id']}")
+        if request['status'] == 'pending':
+            ConnectExtensionProcessorExampleExtension.ACTIVATION_TEMPLATE_NAME = self.config['ACTIVATION_TEMPLATE_NAME']
+            ConnectExtensionProcessorExampleExtension.API_ENDPOINT = self.config['API_ENDPOINT']
+            return Suspend().process_request(request, self.client)
         return ProcessingResponse.done()
 
     def process_asset_resume_request(self, request):
         self.logger.info(f"Obtained request with id {request['id']}")
+        if request['status'] == 'pending':
+            ConnectExtensionProcessorExampleExtension.ACTIVATION_TEMPLATE_NAME = self.config['ACTIVATION_TEMPLATE_NAME']
+            ConnectExtensionProcessorExampleExtension.API_ENDPOINT = self.config['API_ENDPOINT']
+            return Resume().process_request(request, self.client)
         return ProcessingResponse.done()
 
     def process_asset_cancel_request(self, request):
         self.logger.info(f"Obtained request with id {request['id']}")
+        if request['status'] == 'pending':
+            ConnectExtensionProcessorExampleExtension.ACTIVATION_TEMPLATE_NAME = self.config['ACTIVATION_TEMPLATE_NAME']
+            ConnectExtensionProcessorExampleExtension.API_ENDPOINT = self.config['API_ENDPOINT']
+            return Cancel().process_request(request, self.client)
         return ProcessingResponse.done()
 
-    def process_asset_adjustment_request(self, request):
-        self.logger.info(f"Obtained request with id {request['id']}")
-        return ProcessingResponse.done()
-
-    def validate_asset_purchase_request(self, request):
-        self.logger.info(f"Obtained request with id {request['id']}")
-        return ValidationResponse.done(request)
-
-    def validate_asset_change_request(self, request):
-        self.logger.info(f"Obtained request with id {request['id']}")
-        return ValidationResponse.done(request)
-
-    
